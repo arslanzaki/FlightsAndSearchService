5a50d5b62bdad7f45d5a04ddfe605bbe0beb1bd0
arslanzaki <arslanzaki223@gmail.com>
2023-08-02 12:31:46 +0500

    Server creation and basic dotenv setup

---
+++ README.md
@@ -0,0 +1,15 @@
+
+
+/
+
+    - src/
+        index.js //server
+        models/
+        conrollers/
+        middlewares/
+        services/
+        utils/
+        config/
+        repository/
+
+    - tests/ [later]
---
+++ src/index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const { PORT } = require("./config/serverConfig");
+
+const setupAndStartServer = async () => {
+  // create the express object
+  const app = express();
+  app.listen(PORT, () => {
+    console.log(`Server Started At Port ${PORT}`);
+  });
+};
+
+setupAndStartServer();
