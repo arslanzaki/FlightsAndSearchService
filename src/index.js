403d64dcaa7f511c6f34f71215a90c086e5a1bc4
arslanzaki <arslanzaki223@gmail.com>
2023-08-07 10:56:33 +0500

    Added Routes To Complete API

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const { PORT } = require("./config/serverConfig");
+const ApiRoutes = require("./routes/index")
 
 const setupAndStartServer = async () => {
   // create the express object
@@ -8,6 +9,8 @@ const setupAndStartServer = async () => {
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({ extended: true }));
+
+  app.use("/api", ApiRoutes)
 
   app.listen(PORT, () => {
     console.log(`Server Started At Port ${PORT}`);
---
+++ src/routes/v1/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const CityController = require("./../../controllers/city-controller");
+
+const router = express.Router();
+router.post("/city", CityController.create);
+
+module.exports = router;
